343518ef92fa732d22b372bd3df3b5f4437164c8
Noah Woodrum <heymymomthinksimcool@gmail.com>
2021-04-16 12:52:09 -0700

    added and commented out next tests

--- __tests__/age.tests.js
+++ __tests__/age.tests.js
@@ -1,4 +1,4 @@
-import Age from '..src/age.js';
+import earthAge from "src/age.js"
 
 describe('Age', () => {
   let spaceAge;
@@ -10,6 +10,24 @@ describe('Age', () => {
 
 
   test ('spaceAge class should construct an object', () => { 
+    expect(spaceAge).toBeDefined()
+  })
+  test ('turns earth age into mercury age', () => { 
+    expect(spaceAge).toBeDefined()
+  })
+  test ('turns earth age into venus age', () => { 
+    expect(spaceAge).toBeDefined()
+  })
+  test ('turns earth age into mars age', () => { 
+    expect(spaceAge).toBeDefined()
+  })
+  test ('turns earth age into jupiter age', () => { 
+    expect(spaceAge).toBeDefined()
+  })
+  test ('should tell user how many years they have left', () => { 
+    expect(spaceAge).toBeDefined()
+  })
+  test ('if older then expectancy, return number of years theyve exceeded.', () => { 
     expect(spaceAge).toBeDefined()
   })
 })
